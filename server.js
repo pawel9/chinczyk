4af9f6e39645858bec38588493022caf78f11d0b
Pawel <trybusp17@op.pl>
2021-05-01 00:14:29 +0200

    4th

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require("express")
 var app = express()
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require('cors');
 var corsOptions = {
